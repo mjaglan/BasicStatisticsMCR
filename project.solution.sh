bfe2f617e0542c63ad09936663e45fa1489a2947
mjaglan <mjaglan@umail.iu.edu>
2017-06-15 10:30:04 -0400

    hadoop-2.6.4: run application jar

---
+++ project.solution.sh
@@ -0,0 +1,36 @@
+myPWD=$(pwd)
+pName="BasicStatisticsMCR"
+HDFS_HOME_PATH=/user/hduser
+
+# clear previous logs
+rm -rf $HADOOP_HOME/logs/userlogs/*
+
+echo "remove all folders"
+hadoop dfs -rmr $HDFS_HOME_PATH/*
+
+echo "Create directory in HDFS for input data"
+hadoop dfs -mkdir $HDFS_HOME_PATH/input
+
+echo "input data: Copy input data to HDFS"
+hadoop dfs -put ./input/input_BasicStatistics.txt $HDFS_HOME_PATH/input/
+
+echo "Check results"
+sleep 5s
+hadoop dfs -ls $HDFS_HOME_PATH/input
+
+echo "================ RUN HADOOP JAR =================="
+sleep 5s
+hadoop jar ./build/libs/$pName.jar edu.indiana.soic.cs.$pName input output
+
+echo "Check results"
+sleep 5s
+hadoop dfs -ls $HDFS_HOME_PATH
+
+# make output dir
+mkdir -p output
+
+#echo "Compare results"
+rm -rf ./output/*
+hadoop dfs -copyToLocal $HDFS_HOME_PATH/* ./output/
+
+cd $myPWD
